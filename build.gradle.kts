21288a80f524d9a1ae447be17f2ea14d57a490a5
abhirajkumarbat-collab <abhirajkumarbat@gmail.com>
2026-09-18 18:39:12 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.google.devtools.ksp) apply false
-    alias(libs.plugins.roborazzi) apply false
-    alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.google.services) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
